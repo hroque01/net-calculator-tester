0a0dfea6f0fa2273ea608546381a5ca7086dc344
Hadson Roque <nineonezero.light@gmail.com>
2023-04-26 13:37:16 +0200

    Add project files.

---
+++ net-calculator-tester/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_calculator_tester
+{
+    internal class Calculator
+    {
+        public float Add(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+
+        public float Subtract(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+
+        public float Divide(float num1, float num2)
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            return num1 / num2;
+        }
+
+        public float Multiply(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
